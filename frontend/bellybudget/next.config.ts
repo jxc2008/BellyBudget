22f846945ad6ec6d69c6985cbc1f294c32dfd9bb
jxc2008 <jxc2008@nyu.edu>
2025-02-09 11:20:02 -0500

    eslint

--- frontend/bellybudget/next.config.ts
+++ frontend/bellybudget/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This option allows production builds to complete even if there are ESLint errors.
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
